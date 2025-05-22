48d03d4fd4b96a8158644ee2fcf73145a507fa34
Lena Kleyn <r1001360@student.thomasmore.be>
2025-05-22 09:00:09 +0200

    start

---
+++ oefening.js
@@ -0,0 +1,34 @@
+/**
+ * DOM
+ */
+const gridContainer = document.getElementById("gridContainer");
+const movieInput = document.getElementById("movieInput");
+const movieCounter = document.getElementById("movieCounter");
+
+const sortButton = document.getElementById("sortButton");
+const upperCaseButton = document.getElementById("upperCaseButton");
+
+/**
+ * Movies
+ */
+const movies = [
+  "The Shawshank Redemption",
+  "The Godfather",
+  "The Dark Knight",
+  "12 Angry Men",
+  "Schindler's List",
+  "The Lord of the Rings The Return of the King",
+  "Pulp Fiction",
+  "The Lord of the Rings The Fellowship of the Ring",
+  "Forrest Gump",
+  "Inception",
+  "The Lord of the Rings The Two Towers",
+  "One Flew Over the Cuckoo's Nest",
+  "Goodfellas",
+  "Star Wars Episode V - The Empire Strikes Back",
+  "The Matrix",
+  "Seven",
+  "The Silence of the Lambs",
+  "It's a Wonderful Life",
+  "The Wizard of Oz",
+];
